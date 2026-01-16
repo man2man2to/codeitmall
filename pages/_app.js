cf8499c0a08cdd815c69553be793ff3b54640dfb
chungman <leecm940603@gmail.com>
2026-01-16 18:36:54 +0900

    fix Pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
-import "@/styles/globals.css";
+import Container from '@/components/Container';
+import Header from '@/components/Header';
+import Head from 'next/head';
+import { ThemeProvider } from '@/lib/ThemeContext';
+import '@/styles/global.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Codeitmall</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ThemeProvider>
+        <Header />
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      </ThemeProvider>
+    </>
+  );
 }
